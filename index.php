6318ab9f6e67a8f440975cc0866611b5ee4fba5d
Corey Sizemore <coreysizemore@gmail.com>
2022-11-21 14:20:58 -0800

    added gulp files
    - added gulp files
    - testing hosting file push

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<title></title>
+
+		<meta charset="UTF-8">
+		<meta name="description" content="Free Web tutorials">
+		<meta name="keywords" content="HTML, CSS, JavaScript">
+		<meta name="author" content="John Doe">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+		<link rel="stylesheet" href="css/style.min.css">
+
+	</head>
 <body>
 <h1>Hello World</h1>
 <p>I'm hosted with GitHub Pages.</p>
+
+
+<script src="js/script.min.js"></script>
+
 </body>
 </html>
